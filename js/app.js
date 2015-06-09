14b2a1583cc8bacba8f0e39dbdd440a5f0870065
jenjiyi <jenny.yamada@gmail.com>
2015-06-08 18:04:55 -0700

    event listeners to js

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('.ryu').mouseenter(function(){
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function(){
+		$('.ryu-still').show();
+		$('.ryu-ready').hide();
+	})
+
+	.mousedown(function() {
+	    // play hadouken sound
+	    $('.ryu-ready').hide();
+	    $('.ryu-throwing').show();
+	    $('.hadouken ').show();
+	    
+	    // show hadouken and animate it to the right of the screen
+	})
+	.mouseup(function() {
+		$('.ryu-ready').show();
+	    $('.ryu-throwing').hide();
+	});
+});
